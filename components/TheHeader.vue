e01904aac91f0681829bead10ec6e9cb43360df6
AlexanderFalkenberg <35661687+AlexanderFalkenberg@users.noreply.github.com>
2021-11-08 20:10:51 +0100

    mobile menu button fix

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,126 +1,126 @@
 <template>
-  <header>
-    <div class="relative bg-white">
-      <div
-        class="
-          flex
-          justify-between
-          items-center
-          max-w-7xl
-          mx-auto
-          px-4
-          py-6
-          sm:px-6
-          md:justify-start md:space-x-10
-          lg:px-8
-        "
-      >
-        <div class="flex justify-start lg:w-0 lg:flex-1">
-          <nuxt-link
-            to="/"
-            class="
-              text-thin
-              font-display
-              text-2xl
-              tracking-tight
-              text-gray-700
-              uppercase
-            "
-            href="#"
-          >
-            <span class="sr-only">Marco Meiwes</span>
-            Marco Meiwes
-          </nuxt-link>
-        </div>
-        <div class="-mr-2 -my-2 md:hidden">
-          <button
-            @click="mobileMenuIsOpen = true"
-            type="button"
-            class="
-              bg-white
-              rounded-md
-              p-2
-              inline-flex
-              items-center
-              justify-center
-              text-gray-400
-              hover:text-gray-500 hover:bg-gray-100
-              focus:outline-none
-              focus:ring-2
-              focus:ring-inset
-              focus:ring-indigo-500
-            "
-            aria-expanded="false"
-          >
-            <span class="sr-only">Open menu</span>
-            <!-- Heroicon name: outline/menu -->
-            <svg
-              class="h-6 w-6"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          </button>
-        </div>
-        <nav class="hidden md:flex space-x-10">
-          <a
-            href="#about"
-            class="text-base font-medium text-gray-500 hover:text-gray-900"
-          >
-            Über mich
-          </a>
+	<header>
+		<div class="relative bg-white">
+			<div
+				class="
+					flex
+					justify-between
+					items-center
+					max-w-7xl
+					mx-auto
+					px-4
+					py-6
+					sm:px-6
+					md:justify-start md:space-x-10
+					lg:px-8
+				"
+			>
+				<div class="flex justify-start lg:w-0 lg:flex-1">
+					<nuxt-link
+						to="/"
+						class="
+							text-thin
+							font-display
+							text-2xl
+							tracking-tight
+							text-gray-700
+							uppercase
+						"
+						href="#"
+					>
+						<span class="sr-only">Marco Meiwes</span>
+						Marco Meiwes
+					</nuxt-link>
+				</div>
+				<div class="-mr-2 -my-2 md:hidden">
+					<button
+						@click="mobileMenuIsOpen = true"
+						type="button"
+						class="
+							bg-white
+							rounded-md
+							p-2
+							inline-flex
+							items-center
+							justify-center
+							text-gray-400
+							hover:text-gray-500 hover:bg-gray-100
+							focus:outline-none
+							focus:ring-2
+							focus:ring-inset
+							focus:ring-indigo-500
+						"
+						aria-expanded="false"
+					>
+						<span class="sr-only">Open menu</span>
+						<!-- Heroicon name: outline/menu -->
+						<svg
+							class="h-6 w-6"
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+							aria-hidden="true"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								d="M4 6h16M4 12h16M4 18h16"
+							/>
+						</svg>
+					</button>
+				</div>
+				<nav class="hidden md:flex space-x-10">
+					<a
+						href="#about"
+						class="text-base font-medium text-gray-500 hover:text-gray-900"
+					>
+						Über mich
+					</a>
 
-          <a
-            href="#testimonials"
-            class="text-base font-medium text-gray-500 hover:text-gray-900"
-          >
-            Kundenmeinungen
-          </a>
+					<a
+						href="#testimonials"
+						class="text-base font-medium text-gray-500 hover:text-gray-900"
+					>
+						Kundenmeinungen
+					</a>
 
-          <a
-            href="#faq"
-            class="text-base font-medium text-gray-500 hover:text-gray-900"
-          >
-            FAQ
-          </a>
-        </nav>
-        <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-          <a
-            href="mailto:marco-meiwes@web.de"
-            class="
-              ml-8
-              whitespace-nowrap
-              inline-flex
-              items-center
-              justify-center
-              px-5
-              py-2
-              border border-transparent
-              shadow-sm
-              rounded-full
-              text-base
-              font-medium
-              text-white
-              bg-gradient-to-r
-              from-purple-600
-              to-indigo-600
-            "
-          >
-            Jetzt kontaktieren
-          </a>
-        </div>
-      </div>
+					<a
+						href="#faq"
+						class="text-base font-medium text-gray-500 hover:text-gray-900"
+					>
+						FAQ
+					</a>
+				</nav>
+				<div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+					<a
+						href="mailto:marco-meiwes@web.de"
+						class="
+							ml-8
+							whitespace-nowrap
+							inline-flex
+							items-center
+							justify-center
+							px-5
+							py-2
+							border border-transparent
+							shadow-sm
+							rounded-full
+							text-base
+							font-medium
+							text-white
+							bg-gradient-to-r
+							from-purple-600
+							to-indigo-600
+						"
+					>
+						Jetzt kontaktieren
+					</a>
+				</div>
+			</div>
 
-      <!--
+			<!--
         Mobile menu, show/hide based on mobile menu state.
 
         Entering: "duration-200 ease-out"
@@ -130,128 +130,128 @@
           From: "opacity-100 scale-100"
           To: "opacity-0 scale-95"
       -->
-      <div
-        class="
-          absolute
-          z-30
-          top-0
-          inset-x-0
-          p-2
-          transition
-          transform
-          origin-top-right
-          md:hidden
-        "
-        :class="{
-          hidden: !mobileMenuIsOpen,
-        }"
-      >
-        <div class="rounded-lg shadow-lg divide-y-2 divide-gray-50 bg-white">
-          <div class="pt-5 pb-6 px-5">
-            <div class="flex items-center justify-between">
-              <div class="font-display">MARCO MEIWES</div>
-              <div class="-mr-2">
-                <button
-                  @click="mobileMenuIsOpen = false"
-                  type="button"
-                  class="
-                    bg-white
-                    rounded-md
-                    p-2
-                    inline-flex
-                    items-center
-                    justify-center
-                    text-gray-400
-                    hover:text-gray-500 hover:bg-gray-100
-                    focus:outline-none
-                    focus:ring-2
-                    focus:ring-inset
-                    focus:ring-indigo-500
-                  "
-                >
-                  <span class="sr-only">Menü schießen</span>
-                  <!-- Heroicon name: outline/x -->
-                  <svg
-                    class="h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
-                </button>
-              </div>
-            </div>
-          </div>
-          <div class="py-6 px-5">
-            <div
-              @click="mobileMenuIsOpen = false"
-              class="grid grid-cols-2 gap-4"
-            >
-              <a
-                href="#about"
-                class="text-base font-medium text-gray-500 hover:text-gray-900"
-              >
-                Über mich
-              </a>
+			<div
+				class="
+					absolute
+					z-30
+					top-0
+					inset-x-0
+					p-2
+					transition
+					transform
+					origin-top-right
+					md:hidden
+				"
+				:class="{
+					hidden: !mobileMenuIsOpen,
+				}"
+			>
+				<div class="rounded-lg shadow-lg divide-y-2 divide-gray-50 bg-white">
+					<div class="pt-5 pb-6 px-5">
+						<div class="flex items-center justify-between">
+							<div class="font-display">MARCO MEIWES</div>
+							<div class="-mr-2">
+								<button
+									@click="mobileMenuIsOpen = false"
+									type="button"
+									class="
+										bg-white
+										rounded-md
+										p-2
+										inline-flex
+										items-center
+										justify-center
+										text-gray-400
+										hover:text-gray-500 hover:bg-gray-100
+										focus:outline-none
+										focus:ring-2
+										focus:ring-inset
+										focus:ring-indigo-500
+									"
+								>
+									<span class="sr-only">Menü schießen</span>
+									<!-- Heroicon name: outline/x -->
+									<svg
+										class="h-6 w-6"
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke="currentColor"
+										aria-hidden="true"
+									>
+										<path
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											stroke-width="2"
+											d="M6 18L18 6M6 6l12 12"
+										/>
+									</svg>
+								</button>
+							</div>
+						</div>
+					</div>
+					<div class="py-6 px-5">
+						<div
+							@click="mobileMenuIsOpen = false"
+							class="grid grid-cols-2 gap-4"
+						>
+							<a
+								href="#about"
+								class="text-base font-medium text-gray-500 hover:text-gray-900"
+							>
+								Über mich
+							</a>
 
-              <a
-                href="#testimonials"
-                class="text-base font-medium text-gray-500 hover:text-gray-900"
-              >
-                Kundenmeinungen
-              </a>
+							<a
+								href="#testimonials"
+								class="text-base font-medium text-gray-500 hover:text-gray-900"
+							>
+								Kundenmeinungen
+							</a>
 
-              <a
-                href="#faq"
-                class="text-base font-medium text-gray-500 hover:text-gray-900"
-              >
-                FAQ
-              </a>
-            </div>
-            <div class="mt-6">
-              <a
-                href="#"
-                class="
-                  w-full
-                  flex
-                  items-center
-                  justify-center
-                  px-4
-                  py-2
-                  border border-transparent
-                  rounded-md
-                  shadow-sm
-                  text-base
-                  font-medium
-                  text-white
-                  bg-indigo-600
-                  hover:bg-indigo-700
-                "
-              >
-                email@marco.com
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </header>
+							<a
+								href="#faq"
+								class="text-base font-medium text-gray-500 hover:text-gray-900"
+							>
+								FAQ
+							</a>
+						</div>
+						<div class="mt-6">
+							<a
+								href="mailto:marco-meiwes@web.de"
+								class="
+									inline-flex
+									bg-gradient-to-r
+									from-purple-600
+									to-indigo-600
+									bg-origin-border
+									px-4
+									py-2
+									border border-transparent
+									text-base
+									font-medium
+									shadow-sm
+									rounded-full
+									text-white
+									hover:from-purple-700 hover:to-indigo-700
+								"
+							>
+								Jetzt kontaktieren
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      mobileMenuIsOpen: false,
-    };
-  },
+	data() {
+		return {
+			mobileMenuIsOpen: false,
+		};
+	},
 };
 </script>
